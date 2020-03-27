86b427d9eaf10e1fb3cf98e2e8c5d633fabd7b49
areyeslo <areyeslo@users.noreply.github.com>
2020-03-27 13:53:32 -0700

    Added test cases to test_remove_french

--- api/tests/python/namex_services/name_request/test_name_analysis_utils.py
+++ api/tests/python/namex_services/name_request/test_name_analysis_utils.py
@@ -10,7 +10,8 @@ from namex.services.name_request.auto_analyse.name_analysis_utils import remove_
                              ("ABC ENGINEERING/CENTRAL CARE CORP.", "ABC ENGINEERING"),
                              ("RE/MAX WALNUT DEVELOPERS", "RE/MAX WALNUT DEVELOPERS"),
                              ('ARMSTRONG 20/20 VISION', 'ARMSTRONG 20/20 VISION'),
-                             ('ARMSTRONG VISION 20/20' , 'ARMSTRONG VISION 20/20')
+                             ('ARMSTRONG VISION 20/20', 'ARMSTRONG VISION 20/20'),
+                             ("ARMSTRONG VISION/2020 ENTERPRISE", "ARMSTRONG VISION")
                          ]
                          )
 def test_remove_french(name, expected):
@@ -19,10 +20,13 @@ def test_remove_french(name, expected):
 
 @pytest.mark.parametrize("name_list, dist_list, desc_list, dist_expected, desc_expected",
                          [
-                             (['PACIFIC', 'BLUE', 'ENTERPRISE'], ['PACIFIC', 'BLUE', 'ENTERPRISE'], ['PACIFIC', 'ENTERPRISE'],
+                             (['PACIFIC', 'BLUE', 'ENTERPRISE'], ['PACIFIC', 'BLUE', 'ENTERPRISE'],
+                              ['PACIFIC', 'ENTERPRISE'],
                               [['PACIFIC', 'BLUE']], [['ENTERPRISE']]),
-                             (['PACIFIC', 'BLUE', 'COAST', 'ENTERPRISE'], ['PACIFIC', 'BLUE', 'COAST', 'ENTERPRISE'], ['PACIFIC', 'COAST', 'ENTERPRISE'],
-                              [['PACIFIC', 'BLUE'], ['PACIFIC', 'BLUE', 'COAST']], [['COAST', 'ENTERPRISE'], ['ENTERPRISE']])
+                             (['PACIFIC', 'BLUE', 'COAST', 'ENTERPRISE'], ['PACIFIC', 'BLUE', 'COAST', 'ENTERPRISE'],
+                              ['PACIFIC', 'COAST', 'ENTERPRISE'],
+                              [['PACIFIC', 'BLUE'], ['PACIFIC', 'BLUE', 'COAST']],
+                              [['COAST', 'ENTERPRISE'], ['ENTERPRISE']])
                          ]
                          )
 def test_list_distinctive_descriptive(name_list, dist_list, desc_list, dist_expected, desc_expected):
