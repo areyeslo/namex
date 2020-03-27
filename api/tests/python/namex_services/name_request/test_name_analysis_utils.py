5d62c29eae2ed3b4b519f9faa0227190dc484811
areyeslo <areyeslo@users.noreply.github.com>
2020-03-27 12:15:11 -0700

    Adding test cases to french

--- api/tests/python/namex_services/name_request/test_name_analysis_utils.py
+++ api/tests/python/namex_services/name_request/test_name_analysis_utils.py
@@ -8,7 +8,9 @@ from namex.services.name_request.auto_analyse.name_analysis_utils import remove_
                              ("CENTRAL CARE CORPORATION/CORPORATION CENTRALE DE SOINS", "CENTRAL CARE CORPORATION"),
                              ("20/20 CAFE & BAKERY", "20/20 CAFE & BAKERY"),
                              ("ABC ENGINEERING/CENTRAL CARE CORP.", "ABC ENGINEERING"),
-                             ("RE/MAX WALNUT DEVELOPERS", "RE/MAX WALNUT DEVELOPERS")
+                             ("RE/MAX WALNUT DEVELOPERS", "RE/MAX WALNUT DEVELOPERS"),
+                             ('ARMSTRONG 20/20 VISION', 'ARMSTRONG 20/20 VISION'),
+                             ('ARMSTRONG VISION 20/20' , 'ARMSTRONG VISION 20/20')
                          ]
                          )
 def test_remove_french(name, expected):
