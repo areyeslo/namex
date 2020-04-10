d4c4c5b15690c2afe4b9b47c67914fa962805d54
areyeslo <areyeslo@users.noreply.github.com>
2020-04-09 18:48:53 -0700

    Fixing typo in previous commit

--- api/namex/services/name_request/name_analysis_builder_v2/name_analysis_builder.py
+++ api/namex/services/name_request/name_analysis_builder_v2/name_analysis_builder.py
@@ -199,8 +199,7 @@ class NameAnalysisBuilder(AbstractNameAnalysisBuilder):
                                                                                             w_desc, list_name, name)
         most_similar_names.extend(
             list({k for k, v in
-                  sorted(dict_highest_counter.items(), key=lambda item: (-item[1], len(item[0])))[t
-                  0:MAX_MATCHES_LIMIT]}))
+                  sorted(dict_highest_counter.items(), key=lambda item: (-item[1], len(item[0])))[0:MAX_MATCHES_LIMIT]}))
 
         for element in most_similar_names:
             response.update({element: dict_highest_detail.get(element, {})})
