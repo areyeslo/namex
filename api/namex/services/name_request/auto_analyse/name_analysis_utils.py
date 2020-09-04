335f621f02b74cd08d148414076c95e3141e9320
areyeslo <areyeslo@users.noreply.github.com>
2020-09-04 13:03:58 -0700

    Search for exact stand-alone words. Removing stemming to avoid any word which is not stand-alone.

--- api/namex/services/name_request/auto_analyse/name_analysis_utils.py
+++ api/namex/services/name_request/auto_analyse/name_analysis_utils.py
@@ -148,7 +148,7 @@ def check_numbers_beginning(syn_svc, tokens):
 def check_synonyms(syn_svc, stand_alone_words, list_dist_words, list_desc_words):
     both_list = list(set(list_dist_words) & set(list_desc_words))
     for word in both_list:
-        if syn_svc.get_word_synonyms(word=word).data or porter.stem(word.lower()) in stand_alone_words:
+        if syn_svc.get_word_synonyms(word=word).data or word.lower() in stand_alone_words:
             list_dist_words.remove(word)
         else:
             list_desc_words.remove(word)
